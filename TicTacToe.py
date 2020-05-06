a22489d298979c0f0095df0c17e039257c3e2cdf
Jessica <jban@sdf.org>
2020-05-06 20:56:43 +0300

    Add comments, shebang line

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
+#! python3
+# TicTacToe.py - A basic Tic-Tac-Toe game.
 
-# In[1]:
-
-
+# Create an empty board
 board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
-
-# In[2]:
-
-
+# Create the board layout (dividers)
 def display_board(board):
     print(board[1]+' ┃'+board[2]+' ┃'+board[3])
     print(''+ '━━''╋'+'━━'+'╋'+ '━━')
@@ -17,37 +12,32 @@ def display_board(board):
     print(''+ '━━''╋'+'━━'+'╋'+ '━━')
     print(board[7]+' ┃'+board[8]+' ┃'+board[9])
 
-
-# In[3]:
-
-
+# Players choose their markers
 def player_input():
     global marker
     global player2
     global player1
     marker = ''
-    
+
     while marker != 'X' and marker != 'O':
         marker = input('Player 1, choose X or O: ')
-    
+
     player1 = marker
-    
+
     if marker == 'X':
         player2 = 'O'
     else:
         player2 = 'X'
-    
+
     print(f'Player 2, you are {player2}. Let us begin!')
 
-
-# In[4]:
 
 
 import random
 
 def who_goes_first():
     x = random.randint(0,1)
-    
+
     if x == 0:
         return('Player 1, you go first!')
     else:
@@ -83,10 +73,10 @@ def open_space(board,position):
 
 def player_choice(board):
     position = 0
-    
+
     while position not in [1,2,3,4,5,6,7,8,9] or not open_space(board, position):
         position = int(input('Choose your next position: (1-9) '))
-        
+
     return position
 
 
@@ -94,7 +84,7 @@ def player_choice(board):
 
 
 def win_check(board,character):
-    
+
     return((board[1] == character and board[2] == character and board[3] == character) ## across the top
     or (board[4] == character and board[5] == character and board[6] == character) ## across the middle
     or (board[7] == character and board[8] == character and board[9] == character) ## across the bottom
@@ -109,7 +99,7 @@ def win_check(board,character):
 
 
 def full_board():
-    
+
     for p in board:
         if ' ' not in board:
             return True
@@ -121,7 +111,7 @@ def full_board():
 
 
 def replay():
-    
+
     return input('Do you want to play again?').lower().startswith('y')
 
 
@@ -140,14 +130,14 @@ while True:
         game = True
     else:
         game = False
-    
+
     while game:
         if turn == 'Player 1, you go first!':
             display_board(board)
             player_choice(board)
             place_marker(board, player1, position)
             diplay_board(board)
-            
+
             if win_check(board, player1):
                 display_board(board)
                 print("Congratulations, you've won the game!")
@@ -163,7 +153,7 @@ while True:
             display_board(board)
             position = player_choice(board)
             place_marker_p2(board, player2, position)
-            
+
             if win_check(board, player2):
                 display_board(board)
                 print("Congratulations, you've won the game!")
@@ -175,7 +165,7 @@ while True:
                     break
                 else:
                     turn = 'Player 2, you go first!'
-                    
+
     if not replay():
         break
 
@@ -187,7 +177,3 @@ while True:
 
 
 # In[ ]:
-
-
-
-
